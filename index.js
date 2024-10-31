bd26b47e30cf9c438de9f0e7d1b2316469ee0660
Walker2269-chichi <langatjeremiah22@gmail.com>
2024-10-31 16:06:18 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    
+        thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+}
+function countDown(s) {
+    if ( s< 0) {
+        console.log("Please provide a positive integer.");
+        return;
+    }
+
+    let number = s;
+
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+
